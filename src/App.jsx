6c89551e82de160801469381d8a6b984d4e05019
deepgit-hub <darkdeepak001@gmail.com>
2026-07-04 14:40:43 +0530

    I added Course Componenet to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import NavBar from './NavBar';
+import Course from './Course';
 
 function App() {
   
 
   return (
-    
-    <NavBar></NavBar>
-
+    <>             //We are using React Fragment here to wrap multiple components
+    <NavBar />    //We are calling the NavBar component here
+    <Course />     //We are calling the Course component here
+    </>   
   );
 }
 
---
+++ src/Course.jsx
@@ -0,0 +1,10 @@
+function Course() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <h3>HTML</h3>
+            <p>This is Deepak HTML Course</p>
+        </div>
+    );
+}
+export default Course;
