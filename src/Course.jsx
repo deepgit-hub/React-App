1bef2bca055c0685f519f59c4ad010fbcb129b27
deepgit-hub <darkdeepak001@gmail.com>
2026-07-04 22:54:02 +0530

    Learining

--- src/Course.jsx
+++ src/Course.jsx
@@ -3,7 +3,7 @@ const course1 = "React";
 function Course() {
     return (
         <div className="card">
-            <img src="{Deepak}" alt="Deepak img" />
+            <img src={Deepak} alt="Deepak img" />
             <h3>{course1}</h3>
             <p>This is Deepak  Course</p>
         </div>
