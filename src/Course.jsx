48330ec46ea7ee3d07921e7358ba2beef5f9a848
deepgit-hub <darkdeepak001@gmail.com>
2026-07-04 22:44:02 +0530

    Just learning

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,9 +1,9 @@
+import Deepak from './Deepak.png';
 const course1 = "React";
-
 function Course() {
     return (
         <div className="card">
-            <img src="" alt="" />
+            <img src="{Deepak}" alt="Deepak img" />
             <h3>{course1}</h3>
             <p>This is Deepak  Course</p>
         </div>
