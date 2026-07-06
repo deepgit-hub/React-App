51ed7e3b25ee0df3f22c78692a3b20ab41ff892a
deepgit-hub <darkdeepak001@gmail.com>
2026-07-06 15:12:07 +0530

    removed props

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,11 +1,4 @@
-import Dee from './assets/Deepak.png'
-const course1 = "React";
-function Course({
-    name = "Deepak",
-    description = "This is a React course",
-    image = Dee,
-    date = "2026-07-01"
-}) {
+function Course() {
     return (
         <div className="card">
             <img src={image}  />
