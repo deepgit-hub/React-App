0674389e01584ae910d7d198dfee7174a555b3f8
deepgit-hub <darkdeepak001@gmail.com>
2026-07-07 20:56:06 +0530

    created array of objects

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,5 +1,16 @@
 function CourseList()
 {
-    return ();
+    
+        const courses = [
+            {
+                num : 1,
+                name : " Speech 1" ,
+                description : "About Soft Skills",
+                image : {photo1} ,
+                date : "2026-07-01" 
+            }
+        ]
+        return ();
+    
 }
 export default CourseList;
