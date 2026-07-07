0890042c01ca493b6f009282de564c70b18d8c95
deepgit-hub <darkdeepak001@gmail.com>
2026-07-07 20:55:47 +0530

    removed condition rendering

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
     <>             
    
     
-    <Course num = "1" name="Speech 1" description ="About Soft Skills"  image={photo1} date="2026-07-01" show={true} />
-    <Course num = "2" name = "Speech 2" description = "About Coding" image={photo2} date="2026-07-02"  show={true} />
-    <Course num = "3" name="Speech 3" description = "About Public Speaking"  image={photo3} date="2026-07-03" show={true} />    
+    <Course num = "1" name="Speech 1" description ="About Soft Skills"  image={photo1} date="2026-07-01"  />
+    <Course num = "2" name = "Speech 2" description = "About Coding" image={photo2} date="2026-07-02"  />
+    <Course num = "3" name="Speech 3" description = "About Public Speaking"  image={photo3} date="2026-07-03"  />    
     
     </>   
   );
