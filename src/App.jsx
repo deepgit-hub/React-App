9e90623d600c31c2b4c388fc0cbd715ba65b2ae4
deepgit-hub <darkdeepak001@gmail.com>
2026-07-07 22:10:04 +0530

    added course

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import CourseList from './CourseList';
 
 
 function App() {
@@ -6,12 +7,7 @@ function App() {
 
   return (
     <>             
-   
-    
-    <Course num = "1" name="Speech 1" description ="About Soft Skills"  image={photo1} date="2026-07-01"  />
-    <Course num = "2" name = "Speech 2" description = "About Coding" image={photo2} date="2026-07-02"  />
-    <Course num = "3" name="Speech 3" description = "About Public Speaking"  image={photo3} date="2026-07-03"  />    
-    
+    <CourseList />
     </>   
   );
   }
