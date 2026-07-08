f4c804f22428e7ff6f246a3a46bbcae8a4581c35
deepgit-hub <darkdeepak001@gmail.com>
2026-07-08 21:12:02 +0530

    added rating

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,11 +1,11 @@
-function Course({name, description, image,id}) {
+function Course({name, description, image,id,rating}) {
     
     return (
     name &&  <div className="card">
             <img src={image}  />
             <h3>{name}</h3>
             <p>{description}</p>
-            
+            <h4>{rating}</h4>
             
             
         </div>
