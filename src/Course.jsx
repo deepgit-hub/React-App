1bc0d86593ff9cd44fa9a39e60a609f3894c5a0d
deepgit-hub <darkdeepak001@gmail.com>
2026-07-12 19:56:58 +0530

    added event

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,14 +1,15 @@
 function Course({name, image,id,rating}) {
-    function thanks(num)
+    function thanks(num , event)
     {
         console.log(name,"Thank You!!! ",num)
+        console.log(event)
     }
     return (
     name &&  <div className="card">
             <img src={image}  />
             <h3>{name}</h3>
             <h4>{rating}</h4>
-            <button onClick={()=>thanks(20)}>Good</button>
+            <button onClick={(event)=>thanks(20,event)}>Good</button>
             
         </div>
     );
