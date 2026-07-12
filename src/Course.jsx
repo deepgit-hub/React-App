fe8220fa398456ea5adeece0161a9e512ce2f422
deepgit-hub <darkdeepak001@gmail.com>
2026-07-12 12:34:59 +0530

    addded a button

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,11 +1,14 @@
 function Course({name, description, image,id,rating}) {
-    
+    function thanks()
+    {
+        console.log("Thank You!!!")
+    }
     return (
     name &&  <div className="card">
             <img src={image}  />
             <h3>{name}</h3>
             <h4>{rating}</h4>
-            
+            <button onClick={thanks}>Good</button>
             
         </div>
     );
